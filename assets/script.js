31d7f135fa99daf077408575c0a796943ab32bf4
Corbin Ball <c.ball@me.com>
2021-09-16 20:09:55 -0500

    added weatherAPI

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,5 @@
 var userFormEl = document.querySelector('#user-form');
-var languageButtonsEl = document.querySelector('#weather-buttons');
+var weatherButtonsEl = document.querySelector('#weather-buttons');
 var nameInputEl = document.querySelector('#cityname');
 var weatherContainerEl = document.querySelector('#weather-container');
 var weatherSearchTerm = document.querySelector('#weather-search-term');
@@ -32,7 +32,7 @@ var buttonClickHandler = function (event) {
 };
 
 var getUserWeather = function (city) {
-  var apiUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat=&lon=&appid=a6915136789a2bd87e9ef2d2e50b32ba' + city + 'lat' & 'lon';
+  var apiUrl = 'api.openweathermap.org/data/2.5/weather?q='+ nameInputEl.val() +'&appid=a6915136789a2bd87e9ef2d2e50b32ba';
 
   fetch(apiUrl)
     .then(function (response) {
@@ -51,58 +51,7 @@ var getUserWeather = function (city) {
     });
 };
 
-var getFeaturedRepos = function (language) {
-  // The `q` parameter is what language we want to query, the `+is:featured` flag adds a filter to return only featured repositories
-  // The `sort` parameter will instruct GitHub to respond with all of the repositories in order by the number of issues needing help
-  var apiUrl = 'https://api.github.com/search/repositories?q=' + language + '+is:featured&sort=help-wanted-issues';
 
-  fetch(apiUrl).then(function (response) {
-    if (response.ok) {
-      response.json().then(function (data) {
-        displayRepos(data.items, language);
-      });
-    } else {
-      alert('Error: ' + response.statusText);
-    }
-  });
-};
-
-var displayRepos = function (repos, searchTerm) {
-  if (repos.length === 0) {
-    repoContainerEl.textContent = 'No repositories found.';
-    // Without a `return` statement, the rest of this function will continue to run and perhaps throw an error if `repos` is empty
-    return;
-  }
-
-  repoSearchTerm.textContent = searchTerm;
-
-  for (var i = 0; i < repos.length; i++) {
-    // The result will be `<github-cityname>/<github-repository-name>`
-    var repoName = repos[i].owner.login + '/' + repos[i].name;
-
-    var repoEl = document.createElement('div');
-    repoEl.classList = 'list-item flex-row justify-space-between align-center';
-
-    var titleEl = document.createElement('span');
-    titleEl.textContent = repoName;
-
-    repoEl.appendChild(titleEl);
-
-    var statusEl = document.createElement('span');
-    statusEl.classList = 'flex-row align-center';
-
-    if (repos[i].open_issues_count > 0) {
-      statusEl.innerHTML =
-        "<i class='fas fa-times status-icon icon-danger'></i>" + repos[i].open_issues_count + ' issue(s)';
-    } else {
-      statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>";
-    }
-
-    repoEl.appendChild(statusEl);
-
-    repoContainerEl.appendChild(repoEl);
-  }
-};
 
 userFormEl.addEventListener('submit', formSubmitHandler);
 languageButtonsEl.addEventListener('click', buttonClickHandler);
