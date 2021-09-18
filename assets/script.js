62dd88534097b0732747d27fe0f0be7372fc9779
Corbin Ball <c.ball@me.com>
2021-09-17 21:01:10 -0500

    trying to make fetch work. having issue getting anything to show with console.log

--- assets/script.js
+++ assets/script.js
@@ -1,56 +1,24 @@
 var userFormEl = document.querySelector('#user-form');
+var submitBtn = document.querySelector('#submit-btn')
 var weatherButtonsEl = document.querySelector('#weather-buttons');
 var nameInputEl = document.querySelector('#cityname');
 var weatherContainerEl = document.querySelector('#weather-container');
 var weatherSearchTerm = document.querySelector('#weather-search-term');
+console.log(test2);
+console.log("test3");
 
-var formSubmitHandler = function (event) {
-  event.preventDefault();
+var getUserWeather = function () {
+  var apiUrl = 'https://api.openweathermap.org/data/2.5/weather?q='+ nameInputEl.value +'&appid=a6915136789a2bd87e9ef2d2e50b32ba';
 
-  var cityname = nameInputEl.value.trim();
-
-  if (cityname) {
-    getUserWeather(cityname);
-
-    weatherContainerEl.textContent = '';
-    nameInputEl.value = '';
-  } else {
-    alert('Please enter another city');
-  }
-};
-
-var buttonClickHandler = function (event) {
-
-  var weather = event.target.getAttribute('data-weather');
-
-  if (weather) {
-    getFeaturedWeather(weather);
-
-    weatherContainerEl.textContent = '';
-  }
-};
-
-var getUserWeather = function (city) {
-  var apiUrl = 'api.openweathermap.org/data/2.5/weather?q='+ nameInputEl.val() +'&appid=a6915136789a2bd87e9ef2d2e50b32ba';
+  console.log(apiUrl);
 
   fetch(apiUrl)
-    .then(function (response) {
-      if (response.ok) {
-        console.log(response);
-        response.json().then(function (data) {
-          console.log(data);
-          displayRepos(data, city);
-        });
-      } else {
-        alert('Error: ' + response.statusText);
-      }
-    })
-    .catch(function (error) {
-      alert('Unable to get the weather out there');
-    });
-};
+  .then(response => response.json())
+  .then(data => console.log(data))
 
+.catch(err => alert("Cant get the weather here!"))
 
-
-userFormEl.addEventListener('submit', formSubmitHandler);
-languageButtonsEl.addEventListener('click', buttonClickHandler);
+}
+console.log(getUserWeather);
+submitBtn.addEventListener('click', getUserWeather);
+console.log("test");
